40df6cb4bd5186d38aa340fad9b6e72b97d3bf6f
Omar Reda <omarredaelsayied@gmail.com>
2025-03-08 17:25:47 +0200

    spring boot security start

---
+++ springsecSection1/src/test/java/com/eazybytes/springsecSection1/SpringsecSection1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecSection1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecSection1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
