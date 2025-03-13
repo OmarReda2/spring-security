55e0c70ed350b62d24c61db9e159dbf4d4f682b8
Omar Reda <omarredaelsayied@gmail.com>
2025-03-13 11:14:07 +0200

    spring security section 1

---
+++ springsecSection1/src/main/java/com/eazybytes/springsecSection1/EazyBankBackendApplication.java
@@ -0,0 +1,15 @@
+package com.eazybytes.springsecSection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan("com.eazybytes.springsecSection1.controller")  // if the package is not in the same package of the application.java(main package)
+public class EazyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBankBackendApplication.class, args);
+	}
+
+}
